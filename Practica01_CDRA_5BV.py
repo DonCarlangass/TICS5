c2fd1e25d55fd88827de327d87de5e6221e393a6
DonCarlangass <125981876+DonCarlangass@users.noreply.github.com>
2024-10-15 09:49:47 -0600

    Add files via upload

---
+++ Practica01_CDRA_5BV.py
@@ -0,0 +1,9 @@
+#Primer Programa en Phyton
+print ("CAPACITACION DE TICS")
+print ("PROGRAMACION")
+print ("MI PRIMER PROGRAMA EN PHYTON")
+nombre = input ("Nombre:")
+apellidos = input ("Apellidos:")
+plantel = input ("Plantel:")
+print(nombre, apellidos)
+print(plantel)
